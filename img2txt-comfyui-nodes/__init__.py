8ee9d376db6791f70c36cd85152d76676590f9f6
ducido <nduc90313@gmail.com>
2024-08-25 00:12:25 +0700

    remove img2txt

--- img2txt-comfyui-nodes/__init__.py
+++
@@ -1,9 +0,0 @@
-from .src.img2txt_node import Img2TxtNode
-
-NODE_CLASS_MAPPINGS = {
-    "img2txt BLIP/Llava Multimodel Tagger": Img2TxtNode,
-}
-NODE_DISPLAY_NAME_MAPPINGS = {
-    "img2txt BLIP/Llava Multimodel Tagger": "Image to Text - Auto Caption"
-}
-WEB_DIRECTORY = "./web"
